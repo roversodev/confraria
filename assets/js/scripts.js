bf7fd8d703a52ae743cc9cdd3e845c0f47f518e5
Your Name <you@example.com>
2025-02-18 17:27:30 -0300

    script telefone

---
+++ assets/js/scripts.js
@@ -0,0 +1,16 @@
+AOS.init({
+    easing: 'ease-out',
+    duration: 1000,
+    offset: 100
+});
+
+document.getElementById('telefone').addEventListener('input', function(e) {
+  let input = e.target.value;
+  input = input.replace(/\D/g, '');
+  input = input.replace(/(\d{2})(\d)/, '($1) $2');
+  input = input.replace(/(\d{5})(\d)/, '$1-$2');
+  if (input.length > 15) {
+    input = input.slice(0, 15);
+  }
+  e.target.value = input;
+});
